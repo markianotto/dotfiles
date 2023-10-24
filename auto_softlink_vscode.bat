ec9c1cb0c2141b7c1dca35b91834bb66ebf5cb82
mio <marko@tokenworks.com>
2023-10-24 11:16:01 -0400

    add vscode and softlink batch script for windows.

---
+++ auto_softlink_vscode.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Check if a parameter for the Git repository path is provided
+if "%~1"=="" (
+    echo Error: Please provide the path to the Git repository.
+    exit /b 1
+)
+
+:: Set the repository path
+set REPO_PATH=%~1
+
+:: Set the destination folder path
+set DEST_FOLDER=C:\Users\%USERNAME%\AppData\Roaming\Code\User
+
+:: Check if the repository path exists
+if not exist "%REPO_PATH%\vscode\" (
+    echo Error: The folder %REPO_PATH%\vscode\ does not exist.
+    exit /b 1
+)
+
+:: Create symbolic links for keybindings.json and settings.json
+mklink "%DEST_FOLDER%\keybindings.json" "%REPO_PATH%\vscode\keybindings.json"
+if errorlevel 1 (
+    echo Failed to create symbolic link for keybindings.json
+    exit /b 1
+)
+
+mklink "%DEST_FOLDER%\settings.json" "%REPO_PATH%\vscode\settings.json"
+if errorlevel 1 (
+    echo Failed to create symbolic link for settings.json
+    exit /b 1
+)
+
+echo Symbolic links created successfully.
+exit /b 0
