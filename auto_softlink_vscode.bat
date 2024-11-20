2aaa84e12280497cf03576b6ed2c825f3cc6e6cb
mio <marko@tokenworks.com>
2024-11-20 11:42:55 -0500

    add alacritty config

--- auto_softlink_vscode.bat
+++ auto_softlink_vscode.bat
@@ -19,6 +19,12 @@ if not exist "%REPO_PATH%\vscode\" (
     exit /b 1
 )
 
+if exist "%DEST_FOLDER%\keybindings.json" (
+    echo Link already exists: "%DEST_FOLDER%\keybindings.json"
+    echo Deleting existing link...
+    del "%DEST_FOLDER%\keybindings.json"
+)
+
 :: Create symbolic links for keybindings.json and settings.json
 mklink "%DEST_FOLDER%\keybindings.json" "%REPO_PATH%\vscode\keybindings.json"
 if errorlevel 1 (
@@ -26,11 +32,34 @@ if errorlevel 1 (
     exit /b 1
 )
 
+if exist "%DEST_FOLDER%\settings.json" (
+    echo Link already exists: "%DEST_FOLDER%\settings.json"
+    echo Deleting existing link...
+    del "%DEST_FOLDER%\settings.json"
+)
+
 mklink "%DEST_FOLDER%\settings.json" "%REPO_PATH%\vscode\settings.json"
 if errorlevel 1 (
     echo Failed to create symbolic link for settings.json
     exit /b 1
 )
+
+set DEST_FOLDER_ALACRITTY=C:\Users\%USERNAME%\AppData\Roaming\alacritty
+
+mkdir %DEST_FOLDER_ALACRITTY%
+
+if not exist "%DEST_FOLDER_ALACRITTY%" (
+    echo Failed to create the directory for alacritty.
+    exit /b 1
+)
+
+if exist "%DEST_FOLDER_ALACRITTY%\alacritty.toml" (
+    echo Link already exists: "%DEST_FOLDER_ALACRITTY%\alacritty.toml"
+    echo Deleting existing link...
+    del "%DEST_FOLDER_ALACRITTY%\alacritty.toml"
+)
+
+mklink "%DEST_FOLDER_ALACRITTY%\alacritty.toml" "%REPO_PATH%\alacritty\alacritty.toml"
 
 echo Symbolic links created successfully.
 exit /b 0
